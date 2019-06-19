974bc08f4c2435279d0ebe9af4c1a80eee23eff6
cahya dyazin <cahyadyazin@yahoo.com>
2019-06-19 10:25:54 +0700

    up

--- config.php
+++ config.php
@@ -10,7 +10,7 @@
 	define("CONF_LICENSE_URL", 		"https://github.com/twbs/bootstrap/blob/v3-dev/LICENSE");
 	define("CONF_POWERED", 			"BOOTSTRAP 3.4");
 	define("CONF_POWERED_URL", 		"https://getbootstrap.com/docs/3.4/");
-	define("CONF_GITHUB", 			"FULL SOURCE On Github");
-	define("CONF_GITHUB_URL", 		"#");
-	define("CONF_GITHUB_FORK", 		"FORK ME On Github");
-	define("CONF_GITHUB_FORK_URL", 	"#");
+	define("CONF_GITHUB", 			"Full Source On Github");
+	define("CONF_GITHUB_URL", 		"https://github.com/dyazincahya/simple-helpdesk-php/");
+	define("CONF_GITHUB_FORK", 		"Fork Me On Github");
+	define("CONF_GITHUB_FORK_URL", 	"https://github.com/dyazincahya/simple-helpdesk-php/fork");
