4a057290f7bb1f4397faea424e9e43e3b522e9b7
cahya dyazin <cahyadyazin@yahoo.com>
2019-06-25 11:10:33 +0700

    up

--- laporan.php
+++ laporan.php
@@ -1,9 +1,31 @@
-<?php include('header.php');?>
+<?php 
+    include('header.php');
+
+    //date collections
+    $today = date('Y-m-d');
+    $yesterday = date('Y-m-d',strtotime("-1 days"));
+    $last_week = date('Y-m-d',strtotime("-7 days"));
+    $last_month = date('Y-m-d',strtotime("-1 month"));
+    $last_6month = date('Y-m-d',strtotime("-6 month"));
+    $last_year = date('Y-m-d',strtotime("-1 year"));
+    $last_century = date('Y-m-d',strtotime("1900-01-01"));
+?>
 
 <div class="container">
     <div class="page-header print-header"><h1>Laporan</h1></div>
     
     <?php if(!isset($_GET['hideformfilter'])) { ?>
+        <h3>Quick Filter</h3>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$today;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Today</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$yesterday;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Yesterday</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$last_week;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Last Week</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$last_month;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Last Month</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$last_6month;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Last 6 Month</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$last_year;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-default">Last Year</a>
+        <a href="<?=site_url();?>/laporan.php?start=<?=$last_century;?>&end=<?=$today;?>&status=ALL&hideformfilter=true&filter=Filter+Now" class="btn btn-danger">Show All Without Filter</a>
+        
+        <br><br>
+        <h3>Advance Filter</h3>
         <form method="GET">
             <div class="row">
                 <div class="col-md-3 form-group">
@@ -27,7 +49,7 @@
                 </div>
                 <input type="hidden" name="hideformfilter" value="true">
             </div>
-            <input type="submit" name="filter" class="btn btn-primary" value="filter">
+            <input type="submit" name="filter" class="btn btn-primary" value="Filter Now">
         </form>
     <?php } ?>
 
@@ -61,7 +83,12 @@
             $re_end = date("d F Y", strtotime($_GET['end']));
 
             echo '<div class="print-container">';
-            echo '<p>Berikut ini adalah daftar laporan tiket periode <b>'. $re_start .'</b> sampai <b>'. $re_end .'</b> terdapat <span class="label label-default">'. $data_count .' data</span></p>';
+
+            if($_GET['start'] == $last_century){
+                echo '<p>Terdapat <span class="label label-default">'. $data_count .' data</span></p>';
+            } else {
+                echo '<p>Berikut ini adalah daftar laporan tiket periode <b>'. $re_start .'</b> sampai <b>'. $re_end .'</b> terdapat <span class="label label-default">'. $data_count .' data</span></p>';
+            }
 
             echo '
                 <table class="table table-bordered">
@@ -76,17 +103,25 @@
                         <td>TIME</td>
                     </tr>
             ';
-            foreach ($data as $key => $val) {
+            if($data_count !== 0){
+                foreach ($data as $key => $val) {
+                    echo "
+                        <tr>
+                            <td>".($key+1)."</td>
+                            <td>".$val['tu_full_name']." <br> ".$val['tu_email']."</td>
+                            <td>".$val['tt_subject']."</td>
+                            <td>".$val['ts_name']."</td>
+                            <td>".$val['td_name']."</td>
+                            <td>".$val['tp_name']."</td>
+                            <td>".$val['tt_status']."</td>
+                            <td>".date("d F Y", strtotime($val['tt_created']))."<br>".date("H:i:s A", strtotime($val['tt_created']))."</td>
+                        </tr>
+                    ";
+                }
+            } else {
                 echo "
-                    <tr>
-                        <td>".($key+1)."</td>
-                        <td>".$val['tu_full_name']." <br> ".$val['tu_email']."</td>
-                        <td>".$val['tt_subject']."</td>
-                        <td>".$val['ts_name']."</td>
-                        <td>".$val['td_name']."</td>
-                        <td>".$val['tp_name']."</td>
-                        <td>".$val['tt_status']."</td>
-                        <td>".date("d F Y", strtotime($val['tt_created']))."<br>".date("H:i:s A", strtotime($val['tt_created']))."</td>
+                    <tr align='center'>
+                        <td colspan='8'>No data!</td>
                     </tr>
                 ";
             }
